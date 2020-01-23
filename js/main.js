1bd10e9b35b35b781331bc5bf109df6b7f58b049
Yaroslav Vavilin <yaroslavvav56@gmail.com>
2020-01-23 17:39:03 +0500

    сделал адаптив

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.address'),
+    menuItem = document.querySelectorAll('.address_item'),
+    hamburger = document.querySelector('.hamburger');
 
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('address_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('address_active');
+        })
+    })
+});
 
